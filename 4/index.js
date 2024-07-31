a16f500e63aae8de7595c59df2e07b8388985bf8
AMATERASU969 <sloniwe228@mail.ru>
2024-07-31 16:07:48 +0300

    Update index.js

--- 4/index.js
+++ 4/index.js
@@ -2,13 +2,15 @@ function count(){
     
 let a = +prompt("Введите первое число");
 let b = +prompt("Введите второе число");
-setInterval(() =>{
+
     while (a <= b){
-        console.log(a);
-        a += 1;
+        setInterval(() => {
+            console.log(a);
+            a += 1;
+        },1000)
 
     }
-}, 1000)
+
 }
 
 
